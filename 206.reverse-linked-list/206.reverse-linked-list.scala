7de48a91ca5b08cad6f157e9b0c85e894726da3e
antonc27 <antonc27@mail.ru>
2019-08-20 10:29:37 +0000

    206.reverse-linked-list init

---
+++ 206.reverse-linked-list/206.reverse-linked-list.scala
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=206 lang=scala
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (56.27%)
+ * Total Accepted:    655K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def reverseList(head: ListNode): ListNode = {
+        
+    }
+}
