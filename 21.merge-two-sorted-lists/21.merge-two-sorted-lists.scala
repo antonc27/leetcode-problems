179c1a1af99bcd2ead99287804edcde7d83b3e7d
antonc27 <antonc27@mail.ru>
2019-08-24 15:20:25 +0000

    21.merge-two-sorted-lists init

---
+++ 21.merge-two-sorted-lists/21.merge-two-sorted-lists.scala
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=21 lang=scala
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (48.46%)
+ * Total Accepted:    656.6K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def mergeTwoLists(l1: ListNode, l2: ListNode): ListNode = {
+        
+    }
+}
