f2f1a93fcf136ee094495f0be9ec5dd6a349c41c
antonc27 sw <antonc27@mail.ru>
2019-09-08 22:28:26 +0200

    204.count-primes init

---
+++ 204.count-primes/204.count-primes.scala
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=204 lang=scala
+ *
+ * [204] Count Primes
+ *
+ * https://leetcode.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Easy (29.76%)
+ * Total Accepted:    265.2K
+ * Total Submissions: 891.2K
+ * Testcase Example:  '10'
+ *
+ * Count the number of prime numbers less than a non-negative number, n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ * 
+ * 
+ */
+object Solution {
+    def countPrimes(n: Int): Int = {
+        
+    }
+}
