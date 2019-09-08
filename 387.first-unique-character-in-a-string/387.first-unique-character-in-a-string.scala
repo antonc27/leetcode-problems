cf9dc6bcd54fa0af5831ce7f4933041ff45a9b9f
antonc27 sw <antonc27@mail.ru>
2019-09-08 22:18:59 +0200

    387.first-unique-character-in-a-string init

---
+++ 387.first-unique-character-in-a-string/387.first-unique-character-in-a-string.scala
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=387 lang=scala
+ *
+ * [387] First Unique Character in a String
+ *
+ * https://leetcode.com/problems/first-unique-character-in-a-string/description/
+ *
+ * algorithms
+ * Easy (50.61%)
+ * Total Accepted:    312.4K
+ * Total Submissions: 617.3K
+ * Testcase Example:  '"leetcode"'
+ *
+ * 
+ * Given a string, find the first non-repeating character in it and return it's
+ * index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * 
+ * 
+ * 
+ * 
+ * Note: You may assume the string contain only lowercase letters.
+ * 
+ */
+object Solution {
+    def firstUniqChar(s: String): Int = {
+        
+    }
+}
